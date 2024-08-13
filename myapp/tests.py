9429d80df5f1634d2e7b96b3121fb330c45e5fc4
Manjeet Tyagi <mtyagi@indxx.com>
2024-08-13 10:46:45 +0530

    Added Test Case.

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class IndexViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_view(self):
+        # Use reverse to get the URL of the view
+        url = reverse('index')
+        
+        # Send a GET request to the URL
+        response = self.client.get(url)
+        
+        # Check the status code
+        self.assertEqual(response.status_code, 200)
+        
+        # Check the response content
+        self.assertContains(response, "Testing")
